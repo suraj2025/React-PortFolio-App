b53bd336d3cceb57484570efc35a7ee69db28c96
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-24 15:41:40 +0530

    Update intro.js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Typewriter} from 'react-simple-typewriter';
 // import cvFile from './assets/suraj.pdf';
 import './intro.css'
 import bg from '../../assets/image.jpg'
@@ -9,7 +10,19 @@ const intro = () => {
         <section id='intro'>
             <div className="introContent">
             <span className='hello'>Hello</span>
-            <span className='introText'>I'm <span className='introName'>Suraj Kumar</span><br/>Web Designer</span>
+            <span className='introText'>I'm <span className='introName'>Suraj Kumar</span><br/>
+               <Typewriter
+            words={['  Coder', '  Web Developer', '  Hacker']}
+            loop={5}
+            cursor
+            
+            typeSpeed={50}
+            deleteSpeed={30}
+            delaySpeed={1000}
+            // onLoopDone={handleDone}
+            // onType={handleType}
+          />
+            </span>
             <p className='introPara'>I am a skilled web designer with experiance in creating <br/> visually apealling and user-friendly websites.</p>
             <Link to="../assets/suraj.pdf" target="_blank" download><button className='btn'>Download CV</button> </Link>
             </div>
