4789330185686b5f0b0b20414407ce2857fa335b
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-24 14:13:49 +0530

    Update skills.js

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -8,7 +8,7 @@ const skills = () => {
   return (
     <section id='skills'>
       <span className='skillTitle'>What I do</span>
-      <span className='skillDesc'>My name is ABU SAID. I'm a web designer and developer based in Dhaka, Bangladesh.
+      <span className='skillDesc'>My name is Suraj Kumar. I'm a web designer and developer based in Mirzapur, Uttar Pradesh.
 
 I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.</span>
       <div className='skillBars'>
