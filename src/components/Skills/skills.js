abf55a2d454fd8bb97992b9f0b7da1c7002799c4
Suraj Kumar <118501104+suraj2025@users.noreply.github.com>
2024-02-28 21:33:09 +0530

    Add files via upload

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,41 +1,38 @@
-import React from 'react'
-import './skills.css'
-import UIDesign from '../../assets/ui-design.png'
-import WebDesign from '../../assets/website-design.png'
-import AppDesign from '../../assets/app-design.png'
 
-const skills = () => {
+import './skills.css'
+
+import React, { useEffect, useRef } from 'react'
+import Lottie from 'lottie-web'
+import animationData from './about.json';
+
+const Skills = () => {
+  
+  const container = useRef(null);
+  useEffect(() => {
+    Lottie.loadAnimation({
+      container: container.current,
+      renderer: 'svg',
+      // loop: true,
+      autoplay: true,
+      animationData: animationData // Assuming you've imported the animationData correctly
+    });
+  }, []);
   return (
     <section id='skills'>
-      <span className='skillTitle'>What I do</span>
-      <span className='skillDesc'>My name is Suraj Kumar. I'm a web designer and developer based in Mirzapur, Uttar Pradesh.
-
-I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.</span>
-      <div className='skillBars'>
-        <div className='skillBar'>
-          <img src={UIDesign} alt='' className='skillBarImg'/>
-          <div className='skillBarText'>
-            <h2>UI/UX Design</h2>
-            <p>This is a demo text, you can write your own content here.</p>
-          </div>
+      
+      <h2 className='skillTitle'>Who I am</h2>
+      <div className="education-container">
+        <div className="education-content" style={{"letter-spacing": "2px"}}>
+        My name is Suraj Kumar. I'm a web designer and developer based in Mirzapur, Uttar Pradesh.<br /><br />
+        I am a professional and passionate programmer in my daily life. A quick learner with a self-learning attitude. I love to learn and explore new technologies and am Passionate about Problem Solving. Love almost all the stacks of Software Engineering. My current stack includes Nodejs, Nestjs, React, NextJs, Redux, Bootstrap, Ant Design, Material UI, MongoDB, Mysql, PostgreSQL, Firebase, Cloudinary, etc.
+        
+            
+          
         </div>
-        <div className='skillBar'>
-          <img src={WebDesign} alt='' className='skillBarImg'/>
-          <div className='skillBarText'>
-            <h2>Website Design</h2>
-            <p>This is a demo text, you can write your own content here.</p>
-          </div>
-        </div>
-        <div className='skillBar'>
-          <img src={AppDesign} alt='' className='skillBarImg'/>
-          <div className='skillBarText'>
-            <h2>App Design</h2>
-            <p>This is a demo text, you can write your own content here.</p>
-          </div>
-        </div>
+        <div className="animation-container" ref={container}></div>
       </div>
     </section>
   )
 }
 
-export default skills
+export default Skills;
